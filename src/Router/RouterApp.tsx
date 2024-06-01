6a0bf34e8580cc875fc39de5c1c709d08f6a0d5d
Alberto Acevedo <alberto83980654@gmail.com>
2024-06-01 08:52:21 -0600

    adds NavBar

--- src/Router/RouterApp.tsx
+++ src/Router/RouterApp.tsx
@@ -1,15 +1,21 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from '../Components/Home'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../Components/Home";
+import NavBar from "../Shared/NavBar";
 
 const RouterApp = () => {
   return (
     <BrowserRouter>
-    <Routes>
-        <Route path='/' element={ <Home/> } />
-    </Routes>
+      {/* NavBar */}
+      <NavBar
+        child={
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        }
+      />
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default RouterApp
+export default RouterApp;
