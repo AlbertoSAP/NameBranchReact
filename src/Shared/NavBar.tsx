81384cb19b454f32cb73282ea572bad8263506b1
Alberto Acevedo <alberto83980654@gmail.com>
2024-07-20 16:20:53 -0600

    enhance navbar

--- src/Shared/NavBar.tsx
+++ src/Shared/NavBar.tsx
@@ -1,18 +1,23 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import MenuIcon from '@mui/icons-material/Menu';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { AccountCircle, NotInterested } from "@mui/icons-material";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { useContext } from "react";
+import { authContext } from "../Hooks/AuthContext";
 
 interface Props {
   /**
@@ -20,31 +25,88 @@ interface Props {
    * You won't need it on your project.
    */
   window?: () => Window;
-  child:React.ReactElement // este es el tipo que buscabamos
+  child: React.ReactElement; // este es el tipo que buscabamos
 }
 
 const drawerWidth = 240;
-const navItems = ['Home', 'About', 'Contact'];
+const navItems = ["Home", "About", "Contact"];
 
- const NavBar:React.FC<Props> = ({window,child}) =>  {
-
+const NavBar: React.FC<Props> = ({ window, child }) => {
+  const { authInfo, logout } = useContext(authContext)
   const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   const handleDrawerToggle = () => {
     setMobileOpen((prevState) => !prevState);
   };
 
+  const navItemsElements: React.ReactElement[] = [];
+
+  if (authInfo.loginStatus) {
+    navItemsElements.push(
+      <>
+        <IconButton
+          size="large"
+          aria-label="account of current user"
+          aria-controls="menu-appbar"
+          aria-haspopup="true"
+          onClick={handleMenu}
+          color="inherit"
+        >
+          <AccountCircle />{" "}
+          <Typography variant="body1" sx={{ marginLeft: 1 }}>
+            {authInfo.fullName}
+          </Typography>
+        </IconButton>
+        <Menu
+          id="menu-appbar"
+          anchorEl={anchorEl}
+          anchorOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem onClick={handleClose}>Profile</MenuItem>
+          <MenuItem onClick={handleClose}>My account</MenuItem>
+          <MenuItem sx={{color:'red', fontWeight:'bold'}} onClick={()=> logout()}> Logout <NotInterested  sx={{ fontSize:'15px', marginLeft:1 }}/>  </MenuItem>
+        </Menu>
+      </>
+    );
+  }
+
   const drawer = (
-    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
       <Typography variant="h6" sx={{ my: 2 }}>
-        MUI
+        Branch Name
       </Typography>
       <Divider />
       <List>
-        {navItems.map((item) => (
-          <ListItem key={item} disablePadding>
-            <ListItemButton sx={{ textAlign: 'center' }}>
-              <ListItemText primary={item} />
+        {[...navItems, ...navItemsElements].map((item, index) => (
+          <ListItem key={`${index}-item`} disablePadding>
+            <ListItemButton
+              sx={{ textAlign: "center", justifyContent: "center" }}
+            >
+              {typeof item === "string" ? (
+                <ListItemText primary={item} />
+              ) : (
+                item
+              )}
             </ListItemButton>
           </ListItem>
         ))}
@@ -52,10 +114,11 @@ const navItems = ['Home', 'About', 'Contact'];
     </Box>
   );
 
-  const container = window !== undefined ? () => window().document.body : undefined;
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box sx={{ display: "flex" }}>
       <CssBaseline />
       <AppBar component="nav">
         <Toolbar>
@@ -64,23 +127,32 @@ const navItems = ['Home', 'About', 'Contact'];
             aria-label="open drawer"
             edge="start"
             onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: 'none' } }}
+            sx={{ mr: 2, display: { sm: "none" } }}
           >
             <MenuIcon />
           </IconButton>
           <Typography
             variant="h6"
             component="div"
-            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
           >
             Branch Name
           </Typography>
-          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
-            {navItems.map((item) => (
-              <Button key={item} sx={{ color: '#fff' }}>
-                {item}
-              </Button>
-            ))}
+          <Box sx={{ display: { xs: "none", sm: "block" } }}>
+            {[...navItems, ...navItemsElements].map((item, index) => {
+              return (
+                <>
+                  {" "}
+                  {typeof item === "string" ? (
+                    <Button key={`${index}-item`} sx={{ color: "#fff" }}>
+                      {item}
+                    </Button>
+                  ) : (
+                    item
+                  )}
+                </>
+              );
+            })}
           </Box>
         </Toolbar>
       </AppBar>
@@ -94,21 +166,22 @@ const navItems = ['Home', 'About', 'Contact'];
             keepMounted: true, // Better open performance on mobile.
           }}
           sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
           }}
         >
           {drawer}
         </Drawer>
       </nav>
-      <Box component="main" sx={{ p: 3, width:'100%' }}>
+      <Box component="main" sx={{ p: 3, width: "100%" }}>
         <Toolbar />
-        <>
-        {child}
-        </>
+        <>{child}</>
       </Box>
     </Box>
   );
-}
+};
 
-export default NavBar
+export default NavBar;
