cfcbcbd2294b19e974a94eded8b8878b4f4f0490
Alberto Acevedo <alberto83980654@gmail.com>
2024-07-20 16:25:15 -0600

    enhance display login

--- src/Auth/Login.tsx
+++ src/Auth/Login.tsx
@@ -3,6 +3,11 @@ import {
   Button,
   Card,
   CardContent,
+
+  IconButton,
+
+  InputAdornment,
+
   Paper,
   Stack,
   TextField,
@@ -13,21 +18,25 @@ import useScreenSize from "../Hooks/useScreenSize";
 import imgBg from '../source/bgauth.jpg'
 import WhatshotIcon from '@mui/icons-material/Whatshot';
 import { authContext } from "../Hooks/AuthContext";
-import { useContext } from "react";
+import { useContext, useState } from "react";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
  
 const CustomTextField = styled(TextField)({
-    '& .MuiInputBase-input': {
-      fontFamily: 'cursive',  // Cambia esto al tipo de letra que desees
-    },
-    '& .MuiFormLabel-root':{
-      fontFamily: 'cursive',  // Cambia esto al tipo de letra que desees
-    }
-  });
+  "& .MuiInputBase-input": {
+    fontFamily: "cursive", // Cambia esto al tipo de letra que desees
+  },
+  "& .MuiFormLabel-root": {
+    fontFamily: "cursive", // Cambia esto al tipo de letra que desees
+  },
+  "& input[type='password']::-ms-reveal, & input[type='password']::-ms-clear": {
+    display: "none",
+  },
+});
 
 const Login = () => {
 
   const {state, trigger ,setState,setTrigger} = useContext(authContext)
-
+ const [showPassword,setShowPassword] = useState(false)
   const { width } = useScreenSize()
   
   const getResize = (size:number) => {
@@ -53,109 +62,127 @@ const Login = () => {
 
   return (
     <>
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: '100dvh',
-        background: `url(${imgBg})`,
-        backgroundSize:'cover',
-      }}
-    >
-      <Paper sx={{
-        width:getResize(width),
-        borderRadius:'5%',
-        // background:'transparent'
-        opacity: '0.83'
-        }} elevation={16}>
-        <Card
+      <Box
         sx={{
-          borderRadius:'5%',
-          // background:'transparent'
-          opacity: '0.83'
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100dvh",
+          background: `url(${imgBg})`,
+          backgroundSize: "cover",
         }}
-        >
-          <Typography
-            gutterBottom
-            variant="h5"
-            component="div"
-            sx={{ 
-              textAlign: "center", 
-              margin: "1rem",
-              fontFamily:'cursive'
-            }}
-            color="text.secondary"
-          >
-            Login <WhatshotIcon
-            sx={{color:'#cf6c23',
-              top:'3px',
-              position:'relative'
-            }}
-            />
-          </Typography>
-          <CardContent>
-            <Stack
-              component="form"
-              sx={{
-                padding: "1rem",
-              }}
-              spacing={2}
-              noValidate
-              autoComplete="off"
-            >
-              <CustomTextField
-                id="outlined-basic"
-                label="Email"
-                name="email"
-                variant="outlined"  
-                onChange={handerChange}
-              />
-
-              <CustomTextField
-              
-                id="outlined-basic"
-                label="Password"
-                name="password"
-                variant="outlined"
-                onChange={handerChange}
-              />
-            </Stack>
-
-            <Box
-              sx={{
-                display: "flex",
-                justifyContent: "center",
-              }}
-            >
-              <Button variant="contained" color="success"
-              sx={{
-                fontFamily:'cursive'
-              }}
-              onClick={()=> setTrigger({
-                ...trigger,
-                dispatch:true,
-                action:'login'
-              })}
-              >
-                Login
-              </Button>
-            </Box>
-
-          <Typography
+      >
+        <Paper
           sx={{
-            fontFamily:'cursive',
-            textAlign: 'center',
-            marginTop:'2rem'
+            width: getResize(width),
+            borderRadius: "5%",
+            // background:'transparent'
+            opacity: "0.83",
           }}
+          elevation={16}
+        >
+          <Card
+            sx={{
+              borderRadius: "5%",
+              // background:'transparent'
+              opacity: "0.83",
+            }}
           >
-           if you don't have an account yet, click on Register
-          </Typography>
+            <Typography
+              gutterBottom
+              variant="h5"
+              component="div"
+              sx={{
+                textAlign: "center",
+                margin: "1rem",
+                fontFamily: "cursive",
+              }}
+              color="text.secondary"
+            >
+              Login{" "}
+              <WhatshotIcon
+                sx={{ color: "#cf6c23", top: "3px", position: "relative" }}
+              />
+            </Typography>
+            <CardContent>
+              <Stack
+                component="form"
+                sx={{
+                  padding: "1rem",
+                }}
+                spacing={2}
+                noValidate
+                autoComplete="off"
+              >
+                <CustomTextField
+                  id="outlined-basic"
+                  label="Email"
+                  name="email"
+                  variant="outlined"
+                  onChange={handerChange}
+                />
+                {/* convertir en un type Password */}
+                <CustomTextField
+                  type={showPassword ? "text" : "password"}
+                  id="outlined-basic"
+                  label="Password"
+                  name="password"
+                  variant="outlined"
+                  onChange={handerChange}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton
+                          arias-label="toggle password visibility"
+                          onClick={() => setShowPassword(!showPassword)}
+                          onMouseDown={(e) => e.preventDefault()}
+                          edge="end"
+                        >
+                          {showPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              </Stack>
 
-          </CardContent>
-        </Card>
-      </Paper>
-    </Box>
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                }}
+              >
+                <Button
+                  variant="contained"
+                  color="success"
+                  sx={{
+                    fontFamily: "cursive",
+                  }}
+                  onClick={() =>
+                    setTrigger({
+                      ...trigger,
+                      dispatch: true,
+                      action: "login",
+                    })
+                  }
+                >
+                  Login
+                </Button>
+              </Box>
+
+              <Typography
+                sx={{
+                  fontFamily: "cursive",
+                  textAlign: "center",
+                  marginTop: "2rem",
+                }}
+              >
+                if you don't have an account yet, click on Register
+              </Typography>
+            </CardContent>
+          </Card>
+        </Paper>
+      </Box>
     </>
   );
 };
